685c0beaf9e790885938f6f7bd0642f4bca4f287
mike <turnpikemike@gmail.com>
2013-01-11 22:04:44 -0500

    Creating some stumps for the pages.

---
+++ source/portfolio.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML><head>
+    <?php include_once('portfolio/analytics.php') ?>
+    <script src="portfolio/portfolio.js"></script>
+
+</head>
+<body onLoad="moveLinks()">
+<a class="link">photos</a>
+<a class="link" href="apps/appzone.php">apps</a>
+</body>
+</HTML>
---
+++ source/portfolio/portfolio.js
@@ -0,0 +1,7 @@
+
+    function moveLinks(){
+	var l = document.getElementsByClassName('link');
+	for (i = 0; i < l.length; i++){
+		l[i].style.top = 100;
+	}
+}
